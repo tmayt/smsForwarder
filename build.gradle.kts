c92ea4a9918caf1ac462beb77225f5abf43ac977
Tmayt <95756342+tmayt@users.noreply.github.com>
2025-12-27 02:25:51 +0330

    Build and Release

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,10 @@ android {
         }
     }
     
+    lint {
+        checkReleaseBuilds = false
+    }
+    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
