64dae7dd47b33c1948829dd41b13a4dc989da538
GitHub Action <action@github.com>
2025-12-26 23:05:24 +0000

    Bump version to 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.smsforwarder"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
     }
 
     buildTypes {
