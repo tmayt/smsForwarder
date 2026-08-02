ca4252225f06f74bc31aa5bd17b1547375d2fbc6
Tha <95756342+tmayt@users.noreply.github.com>
2026-08-02 12:54:11 +0330

    change for publish app

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.smsforwarder"
+        applicationId = "ir.tmayt.smsforwarder"
         minSdk = 24
         targetSdk = 34
         versionCode = 23
